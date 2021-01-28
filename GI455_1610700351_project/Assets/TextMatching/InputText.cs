434266b5d8edf7d6fd3c04b8711f4c85e1a60b3c
ampwinner <77877732+ampwinner@users.noreply.github.com>
2021-01-29 00:34:46 +0700

    Send Assignment Week1

---
+++ GI455_1610700351_project/Assets/TextMatching/InputText.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputText : MonoBehaviour
+{
+    public string theName;
+    public GameObject inputField;
+    public GameObject textDisplay;
+    private string[] storeName = new string[5] { "Text", "Unity", "Come", "Under", "Pull" };
+
+    public void StoreName()
+    {
+        theName = inputField.GetComponent<Text>().text;
+        if (theName == storeName[0])
+        {
+            textDisplay.GetComponent<Text>().text = "[ " + theName + " ]" + " is found";
+        }
+        if (theName == storeName[1])
+        {
+            textDisplay.GetComponent<Text>().text = "[ " + theName + " ]" + " is found";
+        }
+        if (theName == storeName[2])
+        {
+            textDisplay.GetComponent<Text>().text = "[ " + theName + " ]" + " is found";
+        }
+        if (theName == storeName[3])
+        {
+            textDisplay.GetComponent<Text>().text = "[ " + theName + " ]" + " is found";
+        }
+        if (theName == storeName[4])
+        {
+            textDisplay.GetComponent<Text>().text = "[ " + theName + " ]" + " is found";
+        }
+        else
+        {
+            textDisplay.GetComponent<Text>().text = "[ " + theName + " ]" + " is not found";
+        }
+
+
+    }
+}
